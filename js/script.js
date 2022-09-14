a2dff60bb1858fbf06a73c1843fa683a9b1df71d
EarthDragon10 <lagudadev@gmail.com>
2022-09-14 15:33:21 +0200

    added some rules

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function calcRemainYearsToEighteen(ageInput) {
+	let differenceAge = 18 - ageInput;
+	if (ageInput <= 18) {
+		document.getElementById("box-eighteen-age").innerHTML = differenceAge;
+	} else {
+		document.getElementById("box-eighteen-age").innerHTML = "Hai piú di 18";
+	}
+}
+
+function calcRemainYearsToFifty(ageInput) {
+	let differenceAge = 50 - ageInput;
+	if (ageInput <= 50) {
+		document.getElementById("box-fifty-age").innerHTML = differenceAge;
+	} else {
+		document.getElementById("box-fifty-age").innerHTML = "Hai piú di 50";
+	}
+}
+
+function calcRemainYearsToHundred(ageInput) {
+	let differenceAge = 100 - ageInput;
+
+	ageInput <= 100
+		? (document.getElementById("box-hundred-age").innerHTML = differenceAge)
+		: (document.getElementById("box-hundred-age").innerHTML =
+				"Impossibile");
+}
+
+function getAgeFromInput() {
+	let inputValue = document.getElementById("age").value;
+
+	console.log(inputValue);
+
+	calcRemainYearsToFifty(inputValue);
+	calcRemainYearsToEighteen(inputValue);
+	calcRemainYearsToHundred(inputValue);
+}
